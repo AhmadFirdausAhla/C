ebce8032ce0ebd5d1fdfac0d5f6be74628524b18
unknown <ahmadfirdausahla@gmail.com>
2024-11-26 08:31:44 +0700

    Hello

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your name");
+var name = Console.ReadLine();
+Console.WriteLine("How old are you");
+var old = Console.ReadLine();
+Console.WriteLine("What is your dream");
+var dream = Console.ReadLine();
+Console.Write($"{Environment.NewLine} Hello {name}, you are {old} and i wish you got your dream as {dream}!!");
+Console.Write($"{Environment.NewLine}Press any button to exit");
+Console.ReadKey(true);
